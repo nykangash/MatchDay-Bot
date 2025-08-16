ee3ee42affeab33c22178e7f9564984daf8a047e
Nikan <nykangash@gmail.com>
2025-08-16 19:22:02 +0330

    added more teams more output details added requirements.txt added

--- main.py
+++ main.py
@@ -17,14 +17,26 @@ team_id = {
     "rma": "133738",
     "mnu": "133612",
     "psg": "133714",
-    "bay": "133664"
+    "bay": "133664",
+    "est": "139012",
+    "prs": "139013",
+    "fcb": "133739",
+    "mnc": "133613",
+    "ars": "133604",
+    "tot": "133616",
+    "acm": "133667",
+    "int": "133681",
+    "bvb": "133650",
+    "atm": "133729",
+    "juv": "133676",
+    "nap": "133670"
 }
 # Persian weekday names mapped to JalaliDate.weekday() output (0=شنبه)
 rooz = {
     0: "شنبه",
     1: "یکشنبه",
     2: "دوشنبه",
-    3: "سه‌شنبه",
+    3: "سه‌ شنبه",
     4: "چهارشنبه",
     5: "پنجشنبه",
     6: "جمعه"
@@ -57,38 +69,105 @@ def next_match_finder(team):
         
     cleared_data = data["events"][0]
     
+    
+    game_league = cleared_data["strLeague"]
+    
+    if cleared_data["intRound"] == "200":
+        game_round =  "فینال"
+    elif cleared_data["intRound"] == "150":
+        game_round = "نیمه نهایی"
+    elif cleared_data["intRound"] == "125":
+        game_round = "یک چهارم نهایی" 
+    elif "League" in cleared_data["strLeague"] or "Ligue" in cleared_data["strLeague"] or "Liga" in cleared_data["strLeague"]:
+        game_round = f"هفته {cleared_data["intRound"]} "
+    else:
+        game_round = cleared_data["intRound"]
+    
+    
     next_game = cleared_data["strEvent"]
     next_game_datetime = timezone_convert(cleared_data["strTimestamp"])
     
     weekday, next_game_date = date_converter(cleared_data["strTimestamp"])
     formatted_date = next_game_date.strftime("%B %d") 
-
+    stadium = cleared_data["strVenue"]
     return (
-        f"{next_game}\n"
-        f"{rooz[weekday]}\n"
+        f"تیم های: {next_game}\n"
+        f"مسابقات: {game_league}\n" 
+        f"دور: {game_round}\n"
+        f"روز: {rooz[weekday]}\n"
         f"تاریخ: {formatted_date}\n"
-        f"ساعت: {next_game_datetime.strftime('%H:%M')}"
+        f"ساعت: {next_game_datetime.strftime('%H:%M')}\n"
+        f"ورزشگاه: {stadium}"
     )
 
+
+#   SPAIN
+@bot.command()
+async def atm(ctx):
+    await ctx.send(next_match_finder("atm"))
 @bot.command()
 async def rma(ctx):
     await ctx.send(next_match_finder("rma"))
+@bot.command()
+async def fcb(ctx):
+    await ctx.send(next_match_finder("fcb"))
 
+
+#   ENGLAND
+@bot.command()
+async def ars(ctx):
+    await ctx.send(next_match_finder("ars"))
 @bot.command()
 async def liv(ctx):
     await ctx.send(next_match_finder("liv"))
+@bot.command()
+async def mnu(ctx):
+    await ctx.send(next_match_finder("mnu"))
+@bot.command()
+async def mnc(ctx):
+    await ctx.send(next_match_finder("mnc"))
+@bot.command()
+async def tot(ctx):
+    await ctx.send(next_match_finder("tot"))
 
+
+#   ITALY
+@bot.command()
+async def acm(ctx):
+    await ctx.send(next_match_finder("acm"))
+@bot.command()
+async def inter(ctx):
+    await ctx.send(next_match_finder("int"))
+@bot.command()
+async def nap(ctx):
+    await ctx.send(next_match_finder("nap"))
+@bot.command()
+async def juv(ctx):
+    await ctx.send(next_match_finder("juv"))
+
+#   FRANCE
 @bot.command()
 async def psg(ctx):
     await ctx.send(next_match_finder("psg"))
 
-@bot.command()
-async def mnu(ctx):
-    await ctx.send(next_match_finder("mnu"))
 
+#   GERMANY
+@bot.command()
+async def bvb(ctx):
+    await ctx.send(next_match_finder("bvb"))
 @bot.command()
 async def bay(ctx):
     await ctx.send(next_match_finder("bay"))
+
+
+#   IRAN
+@bot.command()
+async def est(ctx):
+    await ctx.send(next_match_finder("est"))
+@bot.command()
+async def prs(ctx):
+    await ctx.send(next_match_finder("prs"))
+    
 
 
 bot.run()
